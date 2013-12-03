c54a9c9a1dfa4c9fcb8dc4c40dfbc6b5e495219b
OpenShift guest <robinfoxin@gmail.com>
2013-12-02 19:00:39 -0500

    implemented Google API.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
-window.onload = getMyLocation;
-
+var watchId = null;
+var map = null;
 //Wickedy Smart Coords
-
 var ourCoords = {
     latitude: 47.624851,
     longitude: -122.52099
 };
 
 
+window.onload = getMyLocation;
+
+
 function getMyLocation(){
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(displayLocation, displayError);
+        
+        navigator.geolocation.getCurrentPosition(
+        displayLocation, 
+        displayError);
+        
+        var watchButton = document.getElementById("watch");
+        watchButton.onclick = watchLocation;
+        var clearWatchButton = document.getElementById("clearWatch");
+        clearWatchButton.onclick = clearWatch;
     } else {
         alert("Oops, no geolocation support.");
     }
@@ -18,35 +28,22 @@ function getMyLocation(){
     
 }
 
-function displayLocation(position){
+function displayLocation(position) {
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
     
     var div = document.getElementById("location");
     div.innerHTML = "You are at Latitude: " + latitude + ", Longitude: " + longitude;
+    div.innerHTML += " (with " + position.coords.accuracy + " meters accuracy)";
+
     
     var km = computeDistance(position.coords, ourCoords);
     var distance = document.getElementById("distance");
     distance.innerHTML = "You are " + km + " km from the WickedlySmart HQ.";
     
+    if (map === null) {
     showMap(position.coords);
-}
-
-function displayError(error) {
-    var errorTypes = {
-        0: "Unknown error",
-        1: "Position denied by user",
-        2: "Position is not available",
-        3: "Request timed out"
-    };
-    
-    var errorMessage = errorTypes[error.code];
-      if (error.code === 0 || error.code === 2){
-          errorMessage = errorMessage + " " + error.message;
-      }
-    
-    var div = document.getElementById("location");
-    div.innerHTML = error.message;
+    }
 }
 
 //Pre-Baked code: Haversine Equation
@@ -67,27 +64,85 @@ function computeDistance(startCoords, destCoords) {
 
 function degreesToRadians(degrees) {
   var radians = (degrees * Math.PI)/180;
-return radians;
+  return radians;
 }
 
-var map;
+// ------------------ End Ready Bake -----------------
 
 function showMap(coords){
    var googleLatAndLong = 
    new google.maps.LatLng(coords.latitude, 
                           coords.longitude);
+                          
+   var mapOptions = {
+       zoom: 10,
+       center: googleLatAndLong,
+       mapTypeId: google.maps.MapTypeId.ROADMAP
 
-var mapOptions = {
-    zoom: 10,
-    center: googleLatAndLong,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
 };
-var mapDiv = document.getElementById("map");
-map = new google.maps.Map(mapDiv, mapOptions);
+    var mapDiv = document.getElementById("map");
+    map = new google.maps.Map(mapDiv, mapOptions);
+
+    var title = "Your Location";
+    var content = "You are here: " + coords.latitude + ", " + coords.longitude;
+        addMarker(map, googleLatAndLong, title, content);
 }
 
 
+function addMarker(map, latlong, title, content) {
+	var markerOptions = {
+		position: latlong,
+		map: map,
+		title: title,
+		clickable: true
+	};
+	var marker = new google.maps.Marker(markerOptions);
 
+	var infoWindowOptions = {
+		content: content,
+		position: latlong
+	};
+
+	var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+
+	google.maps.event.addListener(marker, 'click', function() {
+		infoWindow.open(map);
+	});
+}
+
+
+function displayError(error) {
+    var errorTypes = {
+        0: "Unknown error",
+        1: "Position denied by user",
+        2: "Position is not available",
+        3: "Request timed out"
+    };
+    
+    var errorMessage = errorTypes[error.code];
+      if (error.code === 0 || error.code === 2){
+          errorMessage = errorMessage + " " + error.message;
+      }
+    
+    var div = document.getElementById("location");
+    div.innerHTML = error.message;
+}
+
+
+// Watches the users location
+
+function watchLocation() {
+    watchId = navigator.geolocation.watchPosition(displayLocation,
+                                                  displayError);
+}
+
+
+function clearWatch() {
+    if (watchId) {
+        navigator.geolocation.clearWatch(watchId);
+        watchId = null;
+    }
+}
 
 
 
